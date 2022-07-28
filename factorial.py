8b88e807b8beef9b7f20e402653610a3505f6cba
uvjour <uvjour@gmail.com>
2022-07-28 00:27:52 -0400

    init

---
+++ factorial.py
@@ -0,0 +1,13 @@
+while True:
+    number = int(input("Please type in a number: "))
+    count = number
+    factor = 1
+    if number <= 0:
+        print("Thanks and bye!")
+        break
+
+    while number > 0:
+        factor *= number
+        number -=1
+    
+    print(f"The factorial of the number {count} is {factor}")
